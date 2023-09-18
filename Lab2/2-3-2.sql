d095d41d42aa7d3b096d0e97546204ce5dbdaee6
SoChiTs <114041454+SoChiTs@users.noreply.github.com>
2023-09-18 11:23:56 +0800

    Add files via upload

---
+++ Lab2/2-3-2.sql
@@ -0,0 +1,6 @@
+CREATE TABLE Department (
+    Dep_code CHAR(4),
+    Dep_name NVARCHAR(10) NOT NULL,
+    Dep_head NVARCHAR(10) NOT NULL,
+    PRIMARY KEY(Dep_code)
+);
---
+++ Lab2/2-3-ex1.sql
@@ -0,0 +1,6 @@
+CREATE TABLE Student 
+    (stu_id CHAR(4),
+    stu_name NVARCHAR(10) NOT NULL,
+    Dep_code CHAR(4)NOT NULL,
+	PRIMARY KEY(stu_id)
+);
